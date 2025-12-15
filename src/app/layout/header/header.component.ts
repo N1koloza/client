cde6242c3e7b5a2b547f5884ead683f09a3de922
nika.kachashvili <Nika.KACHASHVILI@akkodis.com>
2025-12-15 15:12:13 +0100

    -  update header component with Angular Material and Tailwind CSS styles

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+ 
+ 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [ MatBadgeModule, MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+ 
 }
