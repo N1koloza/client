ed6387aae03de6f036ad5feea96190e4bc9ef91c
nika.kachashvili <Nika.KACHASHVILI@akkodis.com>
2025-12-24 14:55:28 +0100

    feat: add empty product details view and routing functionality

--- src/app/features/shop/product-item/product-item.component.ts
+++ src/app/features/shop/product-item/product-item.component.ts
@@ -4,10 +4,17 @@ import { MatCard, MatCardContent } from '@angular/material/card';
 import { MatButton } from '@angular/material/button';
 import { CurrencyPipe } from '@angular/common';
 import { MatIcon } from "@angular/material/icon";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
-  imports: [CurrencyPipe, MatCard, MatCardContent, MatButton, MatIcon],
+  imports: [
+    CurrencyPipe, 
+    MatCard, 
+    MatCardContent, 
+    MatButton, 
+    MatIcon,
+    RouterLink],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss',
 })
