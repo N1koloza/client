fdda59444e1bc1514a24be7150e1cb804d883541
nika.kachashvili <Nika.KACHASHVILI@akkodis.com>
2025-12-26 15:07:03 +0100

    - update Not Found component with enhanced layout and styling

--- src/app/shared/components/not-found/not-found.component.ts
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from "@angular/router";
+import { MatAnchor } from "@angular/material/button";
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [MatIcon, RouterLink, MatAnchor],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
