6d7d17c75f2ec3598acea2e7286ac48e59737a80
nika.kachashvili <Nika.KACHASHVILI@akkodis.com>
2026-01-06 15:19:10 +0100

    - implement cart component with item listing and cart item display

--- src/app/features/cart/cart.component.ts
+++ src/app/features/cart/cart.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../core/services/cart.service';
+import { CartItemComponent } from "../cart-item/cart-item.component";
+import { CartItem } from '../../shared/models/cart';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CartItemComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
 export class CartComponent {
-
+  cartService = inject(CartService);
 }
